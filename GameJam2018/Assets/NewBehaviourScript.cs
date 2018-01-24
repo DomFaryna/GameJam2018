1c7d49ecbc9022787a0e265ee06319f4a9eab2ca
Dom <dominic.faryna@gmail.com>
2018-01-24 18:06:22 -0500

    Sample project

---
+++ GameJam2018/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+		Console.Out.WriteLine("HELLO I AM A GOD!!!");
+		
+	}
+	
+	// Update is called once per frame
+	public void Update ()
+	{
+		int he = 2;
+		Console.Out.WriteLine(++he);
+
+	}
+}
