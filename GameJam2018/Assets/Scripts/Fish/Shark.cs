a1c79441ce088c65e7b394a51d22a5232ff16079
Dom Faryna <dominic.faryna@gmail.com>
2018-01-27 22:17:52 -0500

    fixed shark bug

--- GameJam2018/Assets/Scripts/Fish/Shark.cs
+++ GameJam2018/Assets/Scripts/Fish/Shark.cs
@@ -7,7 +7,7 @@ namespace Fish
     {
         new void Start()
         {
-            stats = new Stats(100f, 10, 10, 10, 10, 10);
+            stats = new Stats(50f, 10, 10, 10, 10, 10);
             base.Start();
         }
 
