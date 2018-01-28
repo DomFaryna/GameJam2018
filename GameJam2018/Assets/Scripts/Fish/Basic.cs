bd40cf374814a35bebbffd835a116af6bf571be0
Dom Faryna <dominic.faryna@gmail.com>
2018-01-27 22:06:48 -0500

    Added basic fish and shark movement

---
+++ GameJam2018/Assets/Scripts/Fish/Basic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+using Random = System.Random;
+
+
+namespace Fish
+{
+    public class Basic :Fish
+    {
+        protected new void Start()
+        {
+            stats = new Stats(100f, 10, 10, 10, 10, 10);
+            cond = Condition.Agressive;
+            base.Start();
+        }
+
+
+        protected override Vector2 findTarget()
+        {
+            Vector2 difference = targetVector - (Vector2)gameObject.GetComponent<Transform>().position;
+            Debug.Log(targetVector + "-" + gameObject.GetComponent<Transform>().position);
+            Debug.Log("Difference!!!!" + difference.ToString());
+            if (difference.magnitude < 5)
+            {
+                Random ran = new Random();
+                Vector2 he = new Vector2(ran.Next(-75, 75), ran.Next(-25, 25));
+                Debug.Log(he);
+                return he;
+            }
+            Debug.Log(difference);
+            return targetVector;
+        }
+
+        protected override void ability()
+        {
+            
+        }
+    }
+}
---
+++ GameJam2018/Assets/Scripts/Fish/Shark.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework.Constraints;
+using UnityEngine;
+
+namespace Fish
+{
+    public class Shark : Fish
+    {
+        new void Start()
+        {
+            stats = new Stats(100f, 10, 10, 10, 10, 10);
+            base.Start();
+        }
+
+        protected override Vector2 findTarget()
+        {
+            return player.GetComponent<Transform>().position;
+        }
+
+        protected override Vector2 calulateMovement(Vector2 target, Vector2 current)
+        {
+            Vector2 returnVector = target - current;
+            return returnVector.normalized;
+        }
+
+        protected override void ability()
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = (findTarget() * 2);
+            gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0f;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.zero);
+        }
+    }
+}
